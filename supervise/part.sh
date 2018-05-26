25edd97c6fe5c5b627d4d2d12d0fd624e3221d71
NagisaZj <1170863106@qq.com>
2018-05-26 08:51:31 +0000

    qwrqr

--- supervise/part.sh
+++ supervise/part.sh
@@ -1,6 +1,6 @@
-#python nocurri_pre.py nocurri_pre
-#python nocurri_nopre.py nocurri_nopre
-python testall.py curri_pre
+python nocurri_nopre.py nocurri_nopre
+python curri_nopre.py curri_nopre
+#python testall.py curri_pre
 python testall.py curri_nopre
-python testall.py nocurri_pre
+#python testall.py nocurri_pre
 python testall.py nocurri_nopre
